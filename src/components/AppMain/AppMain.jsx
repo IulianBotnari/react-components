702b9dbad9322eaa4c15a40796c3fde4e0837e85
IulianBotnari <iulianbotnari93@gmail.com>
2024-11-12 15:09:39 +0100

    Fix Card El

--- src/components/AppMain/AppMain.jsx
+++ src/components/AppMain/AppMain.jsx
@@ -14,10 +14,10 @@ export default function () {
             <main className={MainStyle.main_el}>
                 <div className={CardStyle.card_el}>
                     <AppImg />
-                    <div>
+                    <div className="description">
                         <h3>Titolo del post</h3>
                         <p>
-                            lorem ipsum dolor
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                         </p>
 
 
