d5924addb20bdfec4be0e34aa445f417013f65d2
IulianBotnari <iulianbotnari93@gmail.com>
2024-11-12 15:38:30 +0100

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import './App.css'
 import AppHeader from './components/AppHeader/AppHeader'
 import AppMain from "./components/AppMain/AppMain"
+import AppFooter from "./components/AppFooter/AppFooter"
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
     <>
       <AppHeader />
       <AppMain />
+      <AppFooter />
     </>
   )
 }
